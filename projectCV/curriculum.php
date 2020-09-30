6a894b07ff2ebffe5e7e2b4cfbbaa1d95071bc48
wild-e <wild.emmanuelle@gmail.com>
2020-09-30 21:18:32 +0200

    added features

--- projectCV/curriculum.php
+++ projectCV/curriculum.php
@@ -1,8 +1,67 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./CVstyle.css">
+    <title>Curriculum</title>
+</head>
+<body>
+    
+    <div class="container container-fluid">
+
+        <!----------------------------header--------------------------->
+        <?php include "./header.php";?> 
+
+        <!---------------------------main--------------------------->
+        <main>
+            <h1>Curriculum</h1>
+            
+                <button type="button" class="collapsible"><h2>Rebellatrix quam ut et monstrat.</h2></button>
+                <div class="content">
+                    <p class="text">Tempore quo primis auspiciis in mundanum fulgorem surgeret victura dum erunt homines Roma, ut augeretur sublimibus incrementis, foedere pacis aeternae Virtus convenit atque Fortuna plerumque dissidentes, quarum si altera defuisset, ad perfectam non venerat summitatem.
+                    </p>
+                </div>
+            
+            <section class="background">
+                <h2>Rebellatrix quam ut et monstrat.</h2>
+                <ul>
+                    <li>
+                        <p class="text">hacking</p>
+                        <progress max="100" value="80"></progress>
+                    </li>
+                    <li>
+                        <p class="text">hacking</p>
+                        <progress max="100" value="80"></progress>
+                    </li>            
+                </ul>
+            </section>
+        </main>
+        <!---------------------------footer--------------------------->
+        <?php include "./footer.php";?>
+    </div>
 
 
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script>
+    var coll = document.getElementsByClassName("collapsible");
+var i;
 
-
-
-
-?>
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
+</body>
+</html>

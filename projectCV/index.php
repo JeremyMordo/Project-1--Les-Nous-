f56b0a1507ea327671099a894718c266ca01c312
wild-e <wild.emmanuelle@gmail.com>
2020-09-30 09:46:13 +0200

    rename files projects and contact. Create main element window

--- projectCV/index.php
+++ projectCV/index.php
@@ -3,15 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 <body>
-<!----------------------------menu--------------------------->
-    <?php include "./header.php";?> 
+<!----------------------------header--------------------------->
+<?php include "./header.php";?> 
+
 <!---------------------------contenu--------------------------->
+<main class="container">
+    <div class="window">
+        <button class="btn btn-curriculum" type="button"><a href="./curriculum.php"></a></button>
+        <button class="btn btn-projects" type="button"><a href="./projects.php"></a></button>
+        <button class="btn btn-hobbies" type="button"><a href="./hobbies.php"></a></button>
+        <button class="btn btn-contact" type="button"><a href="./contact.php"></a></button>
+    </div>
+</main>
 
 <!---------------------------footer--------------------------->
     <?php include "./footer.php";?>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>

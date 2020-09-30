1c1c543400bb5db24bf525b0b7e3ee505c9504d6
JeremyMordo <jeje.mordo@orange.fr>
2020-09-30 16:58:08 +0200

    modif index/header/style

--- projectCV/index.php
+++ projectCV/index.php
@@ -9,48 +9,43 @@
 </head>
 
 <body>
-
     <div class="container container-fluid">
-
     <!----------------------------header--------------------------->
         <?php include "./header.php";?> 
-
-
     <!---------------------------contenu--------------------------->
             <div class="window row">
-                <img class="windowback col-12" src="images/window.png">    
-                <div class="cards col-5">
-                    <button class="btn btn-curriculum" type="button"><img alt="curriculum link" src="images/curriculum.png"><a href="./curriculum.php"></a></button>
-                    <div class="card-body">
-                        <h5 class="card-title">Curriculum</h5>
+                <img class="windowback row" src="images/window.png">
+                <div class="cards row">
+                        <button class="card card1 col-6 btn btn-curriculum" type="button"><img alt="curriculum link" src="images/curriculum.png">
+                        <a href="./curriculum.php">
+                            <div class="card-body">
+                                <h5 class="card-title">Curriculum</h5>
+                            </div>
+                        </a>
+                    </button>                  
+                    <div class="card card2 col-6">
+                        <button class="btn btn-projects" type="button"><img alt="projects link" src="images/projects.png"><a href="./projects.php"></a></button>
+                        <div class="card-body">
+                            <h5 class="card-title">Projets</h5>
+                        </div>
                     </div>
+                    <div class="card card3 col-6">
+                        <button class="btn btn-hobbies" type="button"><img alt="hobbies link" src="images/hobbies.png"><a href="./hobbies.php"></a></button>
+                        <div class="card-body">
+                            <h5 class="card-title">Hobbies</h5>
+                        </div>
                     </div>
-                <div class="cards col-5">
-                    <button class="btn btn-projects" type="button"><img alt="projects link" src="images/projects.png"><a href="./projects.php"></a></button>
-                    <div class="card-body">
-                        <h5 class="card-title">Projets</h5>
+                    <div class="card card4 col-6">
+                        <button class="btn btn-contact" type="button"><img alt="contact link" src="images/contact.png"><a href="./contact.php"></a></button>
+                        <div class="card-body">
+                            <h5 class="card-title">Contact</h5>
+                        </div>
                     </div>
-                    </div>
-                </div>
-                <div class="cards col-5">
-                    <button class="btn btn-hobbies" type="button"><img alt="hobbies link" src="images/hobbies.png"><a href="./hobbies.php"></a></button>
-                    <div class="card-body">
-                        <h5 class="card-title">Hobbies</h5>
-                    </div>
-                </div>
-                <div class="cards col-5">
-                    <button class="btn btn-contact" type="button"><img alt="contact link" src="images/contact.png"><a href="./contact.php"></a></button>
-                    <div class="card-body">
-                        <h5 class="card-title">Contact</h5>
-                    </div>
-               </div> 
+                </div> 
             </div>
-
     <!---------------------------footer--------------------------->
     <?php include "./footer.php";?>
     </div>
-
-
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>

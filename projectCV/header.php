de1fbc5882ab4ec792969185d734cab134bdcb81
wild-e <wild.emmanuelle@gmail.com>
2020-09-30 11:54:30 +0200

    added some cool features

--- projectCV/header.php
+++ projectCV/header.php
@@ -1,8 +1,33 @@
-<?php 
+<header>
+  
+    <div class="myInfo">
+        <img class="img-fluid" alt="A photo of HackerMan" src="images/hackerman.png">
+        <ul>
+            <li>HackerMan</li>
+            <li>Hacker presque professionnel</li>
+        </ul>
+    </div>
+    <nav class="navbar navbar-expand-* navbar-dark bg-dark">
+        <a href="./index.php" class="btn btn-outline-light">HackerMan</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="./curriculum.php" class="btn btn-outline-light">Curriculum</a>
+                </li>
+                <li class="nav-item">
+                    <a href="./curriculum.php" class="btn btn-outline-light">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a href="./hobbies.php" class="btn btn-outline-light">Hobbies</a>
+                </li>
+                <li class="nav-item">
+                    <a href="./contact.php" class="btn btn-outline-light">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-
-
-
-
-
-?>
+</header>
--- projectCV/index.php
+++ projectCV/index.php
@@ -7,22 +7,52 @@
     <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
+
 <body>
-<!----------------------------header--------------------------->
-<?php include "./header.php";?> 
 
-<!---------------------------contenu--------------------------->
-<main class="container">
-    <div class="window">
-        <button class="btn btn-curriculum" type="button"><a href="./curriculum.php"></a></button>
-        <button class="btn btn-projects" type="button"><a href="./projects.php"></a></button>
-        <button class="btn btn-hobbies" type="button"><a href="./hobbies.php"></a></button>
-        <button class="btn btn-contact" type="button"><a href="./contact.php"></a></button>
-    </div>
-</main>
+    <div class="container img-fluid">
 
-<!---------------------------footer--------------------------->
+    <!----------------------------header--------------------------->
+        <?php include "./header.php";?> 
+
+
+    <!---------------------------contenu--------------------------->
+        <main>
+            <div class="window">
+
+                <div class="card" style="width: 18rem;">
+                    <button class="btn btn-curriculum" type="button"><img alt="curriculum link" src="images/curriculum.png"><a href="./curriculum.php"></a></button>
+                    <div class="card-body">
+                        <h5 class="card-title">Curriculum</h5>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                <button class="btn btn-projects" type="button"><img alt="projects link" src="images/projects.png"><a href="./projects.php"></a></button>
+                    <div class="card-body">
+                        <h5 class="card-title">Projects</h5>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                <button class="btn btn-hobbies" type="button"><img alt="hobbies link" src="images/hobbies.png"><a href="./hobbies.php"></a></button>
+                    <div class="card-body">
+                        <h5 class="card-title">Projects</h5>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                <button class="btn btn-contact" type="button"><img alt="contact link" src="images/contact.png"><a href="./contact.php"></a></button>
+                    <div class="card-body">
+                        <h5 class="card-title">Projects</h5>
+                    </div>
+                </div>
+            </div>
+    </main>
+
+
+    <!---------------------------footer--------------------------->
     <?php include "./footer.php";?>
+    </div>
+
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
